7b7a26f7134578535f18c9752eb5234e5900400c
Jesse <jesse.impio9@gmail.com>
2025-07-02 20:33:14 +0300

    shop update

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "../style/Shop.css"
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { products } from "../data/Products"
 import { ReactComponent as Cone } from "../images/pine-cone/cone.svg"
 
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 export default function Shop() {
+
+  const gridRef = useRef(null)
+
+  useEffect(() => {
+    ScrollTrigger.batch(".product", {
+      batchMax: 3,
+      onEnter: batch => gsap.to(batch, {opacity: 1, y:-50, stagger: .3}),
+    })
+    
+    
+  }, [])
   return (
     <div className='shop-container'>
       <div className='title-container'>
         <h1>Kauppa</h1>
       </div>
-      <div className='shop-grid'>
+      <div className='shop-grid' ref={gridRef}>
         {products.map((product, index) => (
           <div className='product' key={index}>
             <div className='product-image-wrapper'>
               <img className='product-image' src={product.image} />
               <img className='product-image2' src={product.image2} />
-              {/*<Cone className='cone-icon' />*/}
               <div className='product-info'>
                 <h3>{product.name}</h3>
-                <p>{product.price}e</p>
-                <p>{product.volume}l</p>
+                <div className='e-l'>
+                  <p>{product.price}e</p>
+                  <p>{product.volume}l</p>
+                </div>
               </div>
+              <Cone className='cone-icon' />
             </div>
           </div>
         ))}
